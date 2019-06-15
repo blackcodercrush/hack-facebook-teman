9662a26045a431cbbe1f26f3afcccb94f09a75af
blackcodercrush <45306036+blackcodercrush@users.noreply.github.com>
2019-06-15 14:40:30 +0700

    Add files via upload

---
+++ requests.sh
@@ -0,0 +1,13 @@
+apt update && apt upgrade -y
+pkg install bash -y
+pkg install python2 -y
+pkg install git -y
+pkg install pip 
+pip install --upgrade pip
+pkg install bs4 
+pip2 install requests 
+pkg install mechanize 
+echo
+echo "\033[31;1m Penginstallan Selesai"
+echo
+
