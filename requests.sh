6e5f99202a8f8016904a2b20e83d0407bc6b6d2d
blackcodercrush <45306036+blackcodercrush@users.noreply.github.com>
2019-06-16 11:28:01 +0700

    Add files via upload

---
+++ requests.sh
@@ -0,0 +1,25 @@
+apt update && apt upgrade
+pkg install figlet
+pkg install toilet
+pkg install nano
+pkg install lolcat
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install bash
+pkg install php
+pkg install python
+pkg install python2
+pkg install requests
+pip2 install requests
+pip2 install bs4
+pkg install nmap
+pkg install curl
+pkg install git
+pkg install pip 
+pkg install bs4 
+pip install mechanize 
+echo
+echo "\033[31;1m Penginstallan Selesai"
+echo
+
